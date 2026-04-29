6da28025b54012c8cdbd205e1e8764bef1c8dd95
jlsims4681 <jlsims2@students.wcpss.net>
2026-04-28 21:13:42 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,12 @@
-// This file overwrites the stock UV config.js
-
+/* uv/uv.config.js */
 self.__uv$config = {
-  prefix: "/uv/service/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+    prefix: '/uv/service/',
+    // You can use a public bare server for testing, but host your own for production
+    bare: 'https://tomp.app/', 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: './uv/uv.handler.js',
+    bundle: './uv/uv.bundle.js',
+    config: './uv/uv.config.js',
+    sw: './uv/uv.sw.js',
 };
