6e787ca5d101f52b2773be88ae542cdd057c05b1
jlsims4681 <jlsims2@students.wcpss.net>
2026-04-29 18:19:03 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,3 @@
-/* sw.js */
 importScripts('./uv/uv.bundle.js');
 importScripts('./uv/uv.config.js');
 importScripts('./uv/uv.sw.js');
@@ -6,12 +5,5 @@ importScripts('./uv/uv.sw.js');
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        (async () => {
-            if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
-                return await uv.fetch(event);
-            }
-            return await fetch(event.request);
-        })()
-    );
+    event.respondWith(uv.fetch(event));
 });
